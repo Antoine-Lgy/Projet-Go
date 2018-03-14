d5b92bef1519da58227d526ba6e2850300514b02
Antoine Leguay <antoinelg@hotmail.fr>
2018-03-14 15:37:20 +0100

    Boutons bien placés pour un goban de taille 20x20.

--- Projet Go/src/ihm/DrawGoban.java
+++ Projet Go/src/ihm/DrawGoban.java
@@ -18,41 +18,41 @@ public class DrawGoban extends JPanel implements ActionListener{
 	
 	public void paintComponent(Graphics g){
 		try {
-		Image img = ImageIO.read(new File("/Users/Antoine/Desktop/projet GO/img GO/Goban.png"));//je lis mon image 
+		Image img = ImageIO.read(new File("/Users/Antoine/Desktop/projet GO/img GO/GobanFinal.png"));//je lis mon image 
 		g.drawImage(img, 0, 0, 556, 556, this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		this.setLayout(null);
 		//Create a button on each intersection.
-		/*for(double cpt=0,c=14;cpt<37;cpt++,c=c+20.6){
-			for(double i=0,l=14;i<37;i++,l=l+21.2){
+		for(double cpt=0,c=0;cpt<20;cpt++,c=c+28){
+			for(double i=0,l=0;i<20;i++,l=l+28){
 				JButton goLeftCase= new JButton();
-				//goLeftCase.setOpaque(false);
-				//goLeftCase.setContentAreaFilled(false);
-				goLeftCase.setBorderPainted(false);
-				goLeftCase.setBounds((int) l,(int) c,10,10);
+				goLeftCase.setBorderPainted(false); //Button visibility
+				goLeftCase.setBounds((int) l,(int) c,26,26);
+				//goLeftCase.disable(); //If setBorderPainted is true, buttons are transparent.
 				this.add(goLeftCase);
 				goLeftCase.addActionListener(this);
+				
 			}
-		}*/
+		}
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-		/*Point point=this.getMousePosition();// on recupere la position du click pour y placer la piere 
+		Point point=this.getMousePosition();// on recupere la position du click pour y placer la piere 
 		int x =(int)point.getX();
 		int y=(int)point.getY();
 		try {
 			this.drawStone(x,y);//placer la pierre sur ces coordonner
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		}*/
+		}
 	}
 	
-	/*private void drawStone(int x, int y) throws IOException {
+	private void drawStone(int x, int y) throws IOException {
 		Image white,black ;
 		white= ImageIO.read(new File("/Users/Antoine/Desktop/projet GO/img GO/whitePiece.png"));//image d'une pierre blanche
 		black= ImageIO.read(new File("/Users/Antoine/Desktop/projet GO/img GO/blackPiece.png"));//image d'une pierre noir
@@ -64,6 +64,6 @@ public class DrawGoban extends JPanel implements ActionListener{
 			getGraphics().drawImage(black, x-22, y-22, 40,40, this);
 			tour=true;
 		}
-	}*/
+	}
 	
 }
