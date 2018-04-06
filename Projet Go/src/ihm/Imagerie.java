36ae88db6008bf558e7ea8a7269f183daaf88897
Antoine Leguay <antoinelg@hotmail.fr>
2018-04-06 11:32:59 +0200

    Ajout de la Classe imagerie.

---
+++ Projet Go/src/ihm/Imagerie.java
@@ -0,0 +1,14 @@
+package ihm;
+
+public class Imagerie {
+
+	public static String imgGoban = "/Users/Antoine/Desktop/projet GO/img GO/GobanFinal.png";
+	public static String imgFont = "/Users/Antoine/Desktop/projet GO/img GO/go.jpg";
+	
+	public static String getImgGoban() {
+		return imgGoban;
+	}
+	public static String getImgFont() {
+		return imgFont;
+	}
+}
